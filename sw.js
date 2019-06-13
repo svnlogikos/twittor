8e64e869eeca317c056b5d83f96b7ee4897ea492
Mario Castro Fontemachi <mario.castro@logikos.com.ar>
2019-06-13 09:58:48 -0300

    Modificaciones para IOS 2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts("js/sw-utils.js");
 
 const STATIC_CACHE = "static-v3";
-const DYNAMIC_CACHE = "dynamic-v1";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
